7be139275e3a76499a4849ac2f9f800de158f446
harkato <augustoharuokato@gmail.com>
2025-03-13 13:26:08 -0300

    Exemplo .env

--- src/config/api-config.ts
+++ src/config/api-config.ts
@@ -1,7 +1,7 @@
 const apiConfig = {
   API_URL: import.meta.env.VITE_API_URL || 'http://localhost:8080/msh/spc/v1',
   DASHBOARD_URL: import.meta.env.VITE_DASHBOARD_URL || 'http://localhost:8080/msh/spc/v1/dashboard',
-  API_KEY: import.meta.env.VITE_API_KEY || 'sua-chave-de-api-padrao',
+  API_KEY: import.meta.env.VITE_API_KEY || '0000',
   TIMEOUT: parseInt(import.meta.env.VITE_TIMEOUT || '10000', 10),
   MODE: import.meta.env.MODE, // Modo de execução (development, production, etc.)
 };
